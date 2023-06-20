4faba791f7ee9642c43e9fc2f258475701f26404
Koki Ishii <ereven0330@gmail.com>
2023-06-20 23:31:57 +0900

    PDF表示の追加

---
+++ src/app/components/PdfViewer.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useAppContext } from "@/app/context/store";
+import React from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+export default function PdfViewer(props: any) {
+  const { file } = useAppContext();
+
+  return (
+    <div>
+      {file && (
+        <div>
+          <Document file={file} onLoadSuccess={props.onDocumentLoadSuccess}>
+            <Page pageNumber={props.pageNum} />
+          </Document>
+          <p>
+            Page {props.pageNum} of {props.numPages}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
