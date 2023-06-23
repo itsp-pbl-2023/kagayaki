f1e6e13df3558ae65169683c096a9599ffcfb398
ruuu0048 <kokikokitantan0048@gmail.com>
2023-06-23 16:18:10 +0900

    録音関係できた

--- src/app/api/whisper/route.ts
+++ src/app/api/whisper/route.ts
@@ -1,56 +1,32 @@
 //https://zenn.dev/hathle/books/next-supabase-voice-book/viewer/03_whisper
+//https://javascript.plainenglish.io/transcribe-audio-files-using-whisper-open-ai-api-using-next-js-and-typescript-ad851016c889
 
-import { Configuration, OpenAIApi } from "openai";
-import type { NextApiRequest, NextApiResponse } from "next";
-import formidable, { File } from "formidable";
-import fs from "fs";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-const form = formidable({ multiples: true, keepExtensions: true });
-
-const isFile = (file: File | File[]): file is File => {
-  return !Array.isArray(file) && file?.filepath !== undefined;
-};
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export async function POST(req: NextApiRequest) {
-  //try {
+export async function POST(req: Request) {
   console.log("whisper start");
-  //console.log(req);
-  /*
-  const fileContent: any = await new Promise((resolve, reject) => {
-    form.parse(req, (err, _fields, files) => {
-      if (isFile(files.file)) {
-        resolve(fs.createReadStream(files.file.filepath));
-      }
 
-      return reject("file is not found");
-    });
-  });*/
-  const { fileContent } = req.body.settingsObject;
+  const formData: any = await req.formData();
+  formData.append("model", "whisper-1");
+  formData.append("language", "ja");
+  const audioFile: any = formData.get("file");
+  console.log("audio data", audioFile);
 
-  // Whisper
-  const response = await openai.createTranscription(fileContent, "whisper-1");
-  const transcript = response.data.text;
+  const res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY ?? ""}`,
+    },
+    method: "POST",
+    body: formData,
+  });
 
-  console.log("piyo");
+  const data = await res.json();
+  const transcript = data.text;
 
-  const data = JSON.stringify({
+  console.log("whisper respond");
+  console.log(transcript);
+
+  const responseData = JSON.stringify({
     transcript: `${transcript}`,
   });
-  return new Response(data);
-  //  } catch (error) {
-  //console.error(error);
-  //return reject("whisper error");
-  // }
+
+  return new Response(responseData);
 }
