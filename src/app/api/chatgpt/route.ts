c7eb61e6b01c566fb9ac83a7852eac9a4da93932
aya-se <kakkeno1@gmail.com>
2023-07-04 15:28:27 +0900

    プロンプトの修正

--- src/app/api/chatgpt/route.ts
+++ src/app/api/chatgpt/route.ts
@@ -4,8 +4,7 @@ export async function POST(req: Request) {
   console.log(process.env);
   const prompt = await new Response(req.body).text();
   console.log(prompt);
-  const format = `
-  ### 指示 ###\n以下に入力するプレゼンテーションに対する良い点・悪い点をレビューしてください。出力は以下のJSON形式に沿ってください。\n\n### 出力形式 ###\n{"logic": "{ここに論理性の良さと悪さについて返答を記入}", "explanation": "{ここに内容の説明の良さと悪さについて返答を記入}, "informativeness": "{ここに情報量の良さと悪さについて返答を記入}", "fluency": "{ここに口調の良さと悪さについて返答を記入}"}\n\n### 入力 ###\n`;
+  const format = `### 指示 ###\n以下に入力するプレゼンテーションに対する良い点・悪い点をレビューしてください。出力は以下のJSON形式に沿ってください。\n\n### 出力形式 ###\n{"logic": "{ここに論理性の良さと悪さについて返答を記入}", "explanation": "{ここに内容の説明の良さと悪さについて返答を記入}, "informativeness": "{ここに情報量の良さと悪さについて返答を記入}", "fluency": "{ここに口調の良さと悪さについて返答を記入}"}\n\n### 出力例 ###\n'{"logic": "論理性は高いです。富士山が日本で一番でかい山であり、エベレストが2番目に大きい山という事実に基づいています。また、私が富士山よりもエベレストの方が好きだという主観的な意見も述べられていますが、それについての理由が明確には述べられていません。", "explanation": "内容の説明は中程度です。富士山とエベレストがそれぞれ日本と世界で有名な山であるという事実は述べられていますが、富士山とエベレストの特徴や魅力、なぜエベレストの方が好きなのかについての詳細な説明がないため、内容の充実度はあまり高くないです。", "informativeness": "情報量は低いです。富士山とエベレストがそれぞれ日本で一番でかい山と2番目に大きい山であるという情報は伝えられていますが、それ以外の詳細な情報や裏付けるデータなどは提供されていません。", "fluency": "流暢性は高いです。簡潔な文章でまとまっており、明瞭に意見が表現されています。"}'\n\n### 入力 ###\n`;
 
   const configuration = new Configuration({
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -31,7 +30,7 @@ export async function POST(req: Request) {
           content: format + prompt,
         },
       ],
-      max_tokens: 5000,
+      max_tokens: 2000,
     });
     // GPTの返答を取得
     console.log("動いてますよー");
