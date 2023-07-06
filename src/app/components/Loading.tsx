d00838725cb746cbefd252085ae867e153ed9e8c
aya-se <kakkeno1@gmail.com>
2023-07-07 00:19:00 +0900

    不要なconsole.logの削除

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,6 +1,5 @@
 import styles from "./Loading.module.css";
 export default function Loading() {
-  console.log("loading");
   return (
     <div className={styles.loading}>
       <div className={styles.loading_text}>
